26302638036f0c8ef0b8299b65043508e9f4c7cc
Qualitree-Ltd <info@qualitree.co.uk>
2025-05-25 21:35:49 +0100

    sendQuote.js

---
+++ sendQuote.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://in.xero.com');
+
+  // Your automation code here
+
+  await browser.close();
+})();
